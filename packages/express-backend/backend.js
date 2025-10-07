9d4dcf1e654fb92224f3e8f2a0258f67fb038fbe
Priscilla Garcia <pgarci40@calpoly.edu>
2025-10-07 16:37:36 -0700

    2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,14 +1,26 @@
 import express from "express";
+
 const app = express();
+
 const port = 8000;
+
 app.use(express.json());
 
+const users = {
+    user_list: [
+        {
+            id: "xyz789",
+            name: "Charlie", 
+            job: "janitor"
+        }
+    ]
+};
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send(users);
 });
 
+
 app.listen(port, () => {
-    console.log(
-        `Example app listening at http://localhost:${port}`
-    );
+  console.log(`Example app listening at http://localhost:${port}`);
 });
+
